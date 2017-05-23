368a243bfdfc366ca9344347d80bbdedc4390f82
Nenad Radojevic <nradojevic@evoila.de>
2017-05-23 16:45:14 +0200

    Fix: Rewrite if query in user creation

--- src/scripts/centos/5_configurations.sh
+++ src/scripts/centos/5_configurations.sh
@@ -3,11 +3,15 @@
 ADMIN_PASSWORD=${ADMIN_PASSWORD}
 MASTER_HOSTNAME=$master_hostname
 
-if [ $MASTER_HOSTNAME == $HOSTNAME ]; then
-  rabbitmqctl add_user admin "$ADMIN_PASSWORD"
-  rabbitmqctl set_user_tags admin administrator
-  rabbitmqctl set_permissions admin ".*" ".*" ".*"
+MY_NAME=$(hostname -s)
+
+if [ ! $MASTER_HOSTNAME == $MY_NAME ]; then
+  exit 0
 fi
+
+rabbitmqctl add_user admin "$ADMIN_PASSWORD"
+rabbitmqctl set_user_tags admin administrator
+rabbitmqctl set_permissions admin ".*" ".*" ".*"
 
 # Make all queues highly available
 rabbitmqctl set_policy ha-all ".*" '{"ha-mode":"all"}'
